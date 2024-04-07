c1565788892ade8ce26ccef4944d4eca9349fb05
Munir Sheikh <muneer.m.sheikh@gmail.com>
2024-04-07 23:21:04 +0530

    7Apr24 client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private http: HttpClient){}
+
   title = 'client';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('"https://localhost:5001/api/users').subscribe({
+      next: response => this.users.response ,
+      error: error => console.log(error),
+      complete: () => console.log('request completed')
+    })
+  }
 }
